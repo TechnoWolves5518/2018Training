1946818ac30a5c8e3cd03563302b0244adbdf3d3
boktah <boktah2@gmail.com>
2018-11-15 21:00:13 -0500

    nick just left :(

---
+++ Training/src/org/usfirst/frc/team5518/robot/subsystems/Pathfinding.java
@@ -0,0 +1,75 @@
+package org.usfirst.frc.team5518.robot.subsystems;
+
+import org.usfirst.frc.team5518.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+import jaci.pathfinder.Pathfinder;
+import jaci.pathfinder.Trajectory;
+import jaci.pathfinder.Waypoint;
+import jaci.pathfinder.followers.EncoderFollower;
+import jaci.pathfinder.modifiers.TankModifier;
+
+/**
+ *
+ */
+public class Pathfinding extends Subsystem {
+	
+	private Waypoint[] points = null;
+	private Trajectory.Config config = null;
+	private Trajectory trajectory = null;
+	private TankModifier modifier = null;
+	private Trajectory leftTrajectory, rightTrajectory;
+	EncoderFollower leftFollower, rightFollower;
+    
+    public Pathfinding() {
+    		
+	    points = new Waypoint[] {
+	    		    new Waypoint(-4, -1, Pathfinder.d2r(-45)),      // Waypoint @ x=-4, y=-1, exit angle=-45 degrees
+	    		    new Waypoint(-2, -2, 0),                        // Waypoint @ x=-2, y=-2, exit angle=0 radians
+	    		    new Waypoint(0, 0, 0)                           // Waypoint @ x=0, y=0,   exit angle=0 radians
+	    	};
+	    	
+	    	// Create the Trajectory Configuration
+	    	//
+	    	// Arguments:
+	    	// Fit Method:          HERMITE_CUBIC or HERMITE_QUINTIC
+	    	// Sample Count:        SAMPLES_HIGH (100 000)
+	    	//	                    SAMPLES_LOW  (10 000)
+	    	//	                    SAMPLES_FAST (1 000)
+	    	// Time Step:           0.05 seconds
+	    	// Max Velocity:        1.7 m/s
+	    	// Max Acceleration:    2.0 m/s/s
+	    	// Max Jerk:            60.0 m/s/s/s
+	    config = new Trajectory.Config(Trajectory.FitMethod.HERMITE_CUBIC, Trajectory.Config.SAMPLES_HIGH, 0.02, 1.7, 2.0, 60.0);
+	    	
+	    	// Generate the trajectory
+	    	trajectory = Pathfinder.generate(points, config);
+	    	
+	    	// The distance between the left and right sides of the wheelbase is 0.6m
+	    	double wheelbase_width = 0.6;
+	    	
+	    	// Create the Modifier Object
+	    	modifier = new TankModifier(trajectory);
+	    	modifier.modify(wheelbase_width);
+	    	
+	    	leftTrajectory  = modifier.getLeftTrajectory();       // Get the Left Side
+	    	rightTrajectory = modifier.getRightTrajectory();      // Get the Right Side
+	    	
+	    leftFollower = new EncoderFollower(modifier.getLeftTrajectory());
+	    	rightFollower = new EncoderFollower(modifier.getRightTrajectory());
+    		
+	    	// Encoder Position is the current, cumulative position of your encoder. If you're using an SRX, this will be the
+	    	// 'getEncPosition' function.
+	    	// 1000 is the amount of encoder ticks per full revolution
+	    	// Wheel Diameter is the diameter of your wheels (or pulley for a track system) in meters
+	    	leftFollower.configureEncoder(Robot.sub.getLeftEncoderPos(), 360, Robot.sub.getWheelCircumference());
+	    	rightFollower.configureEncoder(Robot.sub.getRightEncoderPos(), 360, Robot.sub.getWheelCircumference());
+    }
+    
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+    
+}
+
--- Training/src/org/usfirst/frc/team5518/robot/subsystems/RobotSubsystem.java
+++ Training/src/org/usfirst/frc/team5518/robot/subsystems/RobotSubsystem.java
@@ -3,6 +3,9 @@ package org.usfirst.frc.team5518.robot.subsystems;
 import org.usfirst.frc.team5518.robot.RobotMap;
 import org.usfirst.frc.team5518.robot.commands.RobotCommand;
 
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.VictorSP;
 import edu.wpi.first.wpilibj.command.Subsystem;
@@ -14,10 +17,10 @@ import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 public class RobotSubsystem extends Subsystem {
 	
 	// Defines driver motor controllers
-	VictorSP leftRearMotor = new VictorSP(RobotMap.LEFT_REAR_MOTOR);
-	VictorSP rightRearMotor = new VictorSP(RobotMap.RIGHT_REAR_MOTOR);
-	VictorSP leftFrontMotor = new VictorSP(RobotMap.LEFT_FRONT_MOTOR);
-	VictorSP rightFrontMotor = new VictorSP(RobotMap.RIGHT_FRONT_MOTOR);
+	WPI_TalonSRX leftRearMotor = new WPI_TalonSRX(RobotMap.LEFT_REAR_MOTOR);
+	WPI_TalonSRX rightRearMotor = new WPI_TalonSRX(RobotMap.RIGHT_REAR_MOTOR);
+	WPI_TalonSRX leftFrontMotor = new WPI_TalonSRX(RobotMap.LEFT_FRONT_MOTOR);
+	WPI_TalonSRX rightFrontMotor = new WPI_TalonSRX(RobotMap.RIGHT_FRONT_MOTOR);
 	
 	VictorSP topShootMotor = new VictorSP(RobotMap.TOP_SHOOT_MTR);
 	VictorSP btmShootMotor = new VictorSP(RobotMap.BTM_SHOOT_MTR);
@@ -29,8 +32,19 @@ public class RobotSubsystem extends Subsystem {
 	
 	DifferentialDrive driveBase = new DifferentialDrive(leftSide , rightSide); // i did it
     
+	private Encoder leftEnc, rightEnc;
+	// Set up encoder values
+	private double wheelCircumference = 8;
+	private double kDistancePerRevolution = wheelCircumference * Math.PI; // Distance traveled in one wheel rotation (circumference)
+	private double kPulsesPerRevolution = 360; // Encoder pulses in one shaft revolution
+	private double kDistancePerPulse = kDistancePerRevolution / kPulsesPerRevolution; // Distance in inches per pulse
+	
     public RobotSubsystem() {
     		intakeMotor.setInverted(true);
+    		leftEnc.setDistancePerPulse(kDistancePerPulse);
+    		rightEnc.setDistancePerPulse(kDistancePerPulse);
+    		leftEnc.setMaxPeriod(0.1);
+    		rightEnc.setMaxPeriod(0.1);
     }
 
     public void initDefaultCommand() {
@@ -49,6 +63,20 @@ public class RobotSubsystem extends Subsystem {
     public void intake(double speed) {
 		intakeMotor.setSpeed(speed);
     }
+    
+    public int getLeftEncoderPos() {
+    		return leftEnc.get();
+    }
+    
+    public int getRightEncoderPos() {
+		return rightEnc.get();
+    }
+    
+    public double getWheelCircumference() {
+    		// converts inches to meters because pathfinder is dumb
+    		return wheelCircumference * 0.0254;
+    }
+    
 }
 
 
